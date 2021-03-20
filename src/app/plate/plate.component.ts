78847de2e15790b51fdec4f23a58397207bede65
Aditya Bharadwaj <adityabharadwaj26@gmail.com>
2021-03-20 20:17:20 +0530

    Three tabs, video, ip and image

--- src/app/plate/plate.component.ts
+++ src/app/plate/plate.component.ts
@@ -20,6 +20,8 @@ export class PlateComponent implements OnInit {
   canvas: HTMLCanvasElement;
   w;
   h;
+  videoW;
+  videoH;
   ratio;
   imageElement: HTMLImageElement;
   // videoSource;
@@ -41,52 +43,78 @@ export class PlateComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const image = document.querySelector(".imageClass");
+    console.log(image);
+
   }
 
+  tabClick(e) {
+    console.log(e);
+    this.imgResponse = '';
+    this.imgUpload = '';
+    this.form.reset();
+    console.log(e.index);
+    if (e.index === 1) {
+      this.video = document.querySelector('video');
+      console.log(this.video);
+      console.log(this.video.videoHeight);
+      console.log(this.video.videoWidth);
+      this.ratio = this.video.videoWidth / this.video.videoHeight;
+      this.videoW = (this.video.videoWidth - 100);
+      this.videoH = (this.videoW / this.ratio);
+    }
+  }
+  ngAfterViewInit() {
+    // const imageElement = this.imageRef.nativeElement;
+    // console.log(imageElement);
+
+  }
   changeSource() {
     const obj = this.videoSourceform.getRawValue();
     console.log(obj);
     this.videoSourceSubmitted = obj.videoSource;
+    this.imageElement = this.imageRef.nativeElement;
+    console.log(this.imageElement);
+    this.imageElement.crossOrigin = '';
   }
 
-  snap() {
+  snapVideo() {
+    // const video = document.querySelector('video');
+    this.canvas = document.createElement('canvas');
+    this.context = this.canvas.getContext('2d');
+    console.log(this.videoH);
+    console.log(this.videoW);
+    this.canvas.width = this.videoW;
+    this.canvas.height = this.videoH;
+    this.context.drawImage(this.video, 0, 0, this.videoW, this.videoH);
+    const dataURI = this.canvas.toDataURL('image/jpeg', 1);
+    console.log(dataURI);
+    this.imgResponse = dataURI;
+    const base64result = dataURI.split(',')[1];
+    const dataBlob = this.convertBase64(base64result, 'file');
+    this.getPlateInfo(dataBlob);
+  }
+
+  snapIpCamera() {
     // this.context.fillRect(0, 0, this.w, this.h);
+    console.log('snapipcamera');
     this.canvas = document.createElement('canvas');
     this.canvas.width = 640;
     this.canvas.height = 480;
     this.context = this.canvas.getContext('2d');
-    this.w = this.imageRef.nativeElement.width;
-    this.h = this.imageRef.nativeElement.height;
-    this.imageRef.nativeElement.crossOrigin = 'anonymous';
-    this.context.drawImage(this.imageRef.nativeElement, 0, 0, this.w, this.h);
-    const dataURI = this.canvas.toDataURL('image/jpeg', 1);
+    console.log('2');
+    const image: HTMLImageElement = document.querySelector(".imageClass");
+    console.log(image);
+    this.w = image.width;
+    this.h = image.height;
+    image.crossOrigin = 'anonymous';
+    console.log('3');
+    this.context.drawImage(image, 0, 0, this.w, this.h);
+    console.log('3.5');
+    const dataURI = this.canvas.toDataURL('image/png');
+    console.log('4');
     const base64result = dataURI.split(',')[1];
     const dataBlob = this.convertBase64(base64result, 'file');
-    console.log(dataURI);
-    const dataBlob1 = this.canvas.toBlob(function (blob) {
-      console.log(blob.type);
-    }, 'file');
-    console.log(dataBlob1);
-    // console.log(this.imageRef.nativeElement.src);
-    // var link = document.createElement("a");
-    // const dataURI = this.canvas.toBlob((blob) => {
-    //   // canvas.toBlob(function(blob){
-    //   link.href = URL.createObjectURL(blob);
-    //   console.log(blob);
-    //   console.log(link.href); // this line should be here
-    // }, 'image/png')
-    // ); // can also use 'image/png'
-    // this.imgResponse = dataBlob1;
-    // this.imgResponse = dataURI;
-    const reader = new FileReader();
-    let res;
-    reader.addEventListener('loadend', () => {
-      res = reader.result;
-      // this.imgUpload = res;
-    });
-    reader.readAsDataURL(dataBlob);
-
-    console.log(res);
     this.getPlateInfo(dataBlob);
   }
 
